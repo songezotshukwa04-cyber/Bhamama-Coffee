305176ed186afed04f7c1c38ff721a89dd617672
Songezo Tsukwa <songezotshukwa04@gmail.com>
2026-09-06 20:31:44 +0200

    Update Server.js

--- BACKEND/Server.js
+++ BACKEND/Server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const db = require('./db');
+const db = require('./Db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
